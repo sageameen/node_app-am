634dd41db021a7d99e9909b814805ba9ccc0ef97
Anandurajkumar <94917286+Anandurajkumar@users.noreply.github.com>
2022-07-21 15:34:07 +0530

    Update index.js

--- web1/index.js
+++ web1/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node i am AnanduRajkumar booom......booom...jjjj..\n'
+  const msg = 'Hello Node i am AnanduRajkumar booom......booom..yoo..yooo.\n'
   res.end(msg);
 });
 

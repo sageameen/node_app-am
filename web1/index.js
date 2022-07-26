ba170e2f9a54b810dcbea33e27310666fd454cde
sageameen <100512286+sageameen@users.noreply.github.com>
2022-07-26 18:41:21 +0530

    Update index.js

--- web1/index.js
+++ web1/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hellooo world 3 \n build 3'
+  const msg = 'Hellooo world 4 \n build 4'
   res.end(msg);
 });
 

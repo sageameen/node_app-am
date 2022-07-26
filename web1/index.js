cad0b21a90409d020a51431f191894d6140b0eb0
sageameen <100512286+sageameen@users.noreply.github.com>
2022-07-26 17:21:30 +0530

    Update index.js

--- web1/index.js
+++ web1/index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hellooo world\n edited'
+  const msg = 'Hellooo world 2 \n build 2'
   res.end(msg);
 });
 
